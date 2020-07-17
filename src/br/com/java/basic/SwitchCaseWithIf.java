dd53a7f772dd2a77ee96d1bf5eb0ba921ab8299a
Bruno Santos <bruno.santoss@originalapp.com.br>
2020-07-17 09:04:25 -0300

    estrutura while e for

---
+++ src/br/com/java/basic/SwitchCaseWithIf.java
@@ -0,0 +1,36 @@
+package br.com.java.basic;
+
+import java.util.Scanner;
+
+public class SwitchCaseWithIf {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		String dia;
+		
+		if(x == 1) {
+			dia = "Domingo";
+		} else if(x == 2) {
+			dia = "Segunda";
+		} else if(x == 3) {
+			dia = "Terça";
+		} else if(x == 4) {
+			dia = "Quarta";
+		} else if(x == 5) {
+			dia = "Quinta";
+		} else if(x == 6) {
+			dia = "Sexta";
+		} else if(x == 7) {
+			dia = "Sábado";
+		} else {
+			dia = "Dia digitado é inválido.";
+		}
+		
+		System.out.println("Dia da semana: " + dia);
+
+		sc.close();
+	}
+
+}
